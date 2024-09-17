654da230c6004d8583fcd03860813fbf1a5a90eb
Érico Andrei <ericof@gmail.com>
2024-09-17 15:45:03 -0300

    Exercício 29: Adiciona campo Cargo

--- backend/src/portal/governo/content/pessoa.py
+++ backend/src/portal/governo/content/pessoa.py
@@ -1,10 +1,18 @@
 from plone.dexterity.content import Container
 from plone.supermodel import model
+from portal.governo import _
+from zope import schema
 from zope.interface import implementer
 
 
 class IPessoa(model.Schema):
     """Definição de uma Pessoa no governo."""
+
+    cargo = schema.Choice(
+        title=_("Cargo"),
+        vocabulary="portal.governo.vocabulary.cargos",
+        required=False,
+    )
 
 
 @implementer(IPessoa)
