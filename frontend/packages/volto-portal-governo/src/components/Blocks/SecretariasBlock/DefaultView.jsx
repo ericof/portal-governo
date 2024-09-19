8ddccf71e9e10b79636b44e5e15e54f7f2deb926
Érico Andrei <ericof@gmail.com>
2024-09-19 17:06:07 -0300

    Exercício 39.2: Listagem de Secretarias dentro do Bloco

--- frontend/packages/volto-portal-governo/src/components/Blocks/SecretariasBlock/DefaultView.jsx
+++ frontend/packages/volto-portal-governo/src/components/Blocks/SecretariasBlock/DefaultView.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Container } from '@plone/components';
+import { UniversalLink } from '@plone/volto/components';
+
+const SecretariaSummary = ({ content }) => {
+  const { gestor } = content;
+  return (
+    <Container className={`secretaria summary`}>
+      <h4>
+        <UniversalLink item={content}>{content.title}</UniversalLink>
+      </h4>
+      {gestor && <span>{gestor.title}</span>}
+    </Container>
+  );
+};
 
 const SecretariasView = (props) => {
-  const { headline, className } = props;
+  const { secretarias, headline, className } = props;
   return (
     <Container narrow className={`block secretarias ${className}`}>
       <h3>{headline}</h3>
+      <Container className={`secretarias listing`}>
+        {secretarias &&
+          secretarias.length > 0 &&
+          secretarias.map(function (secretaria, i) {
+            return <SecretariaSummary content={secretaria} key={i} />;
+          })}
+      </Container>
     </Container>
   );
 };
